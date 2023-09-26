15cff0317f2e99260d79228ff2d423f74f71b6c4
Jakub Juszczak <netghost03@gmail.com>
2023-09-26 15:54:09 +0200

    fix: fix base url

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -7,6 +7,7 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
   description: 'compentum Hilfeseiten für Benutzer',
+  base: '/compentum-docs/',
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
@@ -28,7 +29,6 @@ module.exports = {
     repo: '',
     editLinks: false,
     docsDir: '',
-    base: '/compentum-docs/',
     editLinkText: '',
     lastUpdated: false,
     sidebar: {
